31b43d27733e62e1c669ff008044e6dc5d6c6d9b
David <davidgcastro93@gmail.com>
2019-01-03 20:08:25 -0500

    Added more notes

--- tests/single_test.js
+++ tests/single_test.js
@@ -1,13 +1,15 @@
 module.exports = {
   name: "Google's Search Functionality",
   run: function(browser) {
-    return browser
-      .get('https://www.google.com/ncr')
-      .elementByName('q')
-      .sendKeys('BrowserStack\n')
-      .sleep(5000)
-      .title()
-      //note this is a promise, we are using chai promise langauge here.
-      .should.become('BrowserStack - Google Search');
+    return (
+      browser
+        .get('https://www.google.com/ncr')
+        .elementByName('q')
+        .sendKeys('BrowserStack\n')
+        .sleep(5000)
+        .title()
+        //note this is a promise, we are using chai promise langauge here.
+        .should.become('BrowserStack - Google Search')
+    );
   }
 };
